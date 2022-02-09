b1b37d2358beea735f9ef7d625b80a6f7394ce51
Magnus Åkerfeldt <magnus@akerfeldt.net>
2022-02-09 20:21:35 +0100

    new push

--- src/main/code/ContinuousIntegrationServer.java
+++ src/main/code/ContinuousIntegrationServer.java
@@ -10,8 +10,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import java.util.stream.Collectors;
-
 
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -45,8 +43,9 @@ public class ContinuousIntegrationServer extends AbstractHandler
             System.out.println("Code compilation failed.");
         }
 
-        //System.out.println(JSONtoString(request));
-        String branchName = getBranchName(request);
+        // Print branch name of the commit
+        String JSONstring = Functions.JSONtoString(request);
+        String branchName = Functions.getBranchName(JSONstring);
         System.out.println("Branch name of commit: " + branchName);
 
 
@@ -54,30 +53,6 @@ public class ContinuousIntegrationServer extends AbstractHandler
         //System.out.println(result);
 
         response.getWriter().println("CI job done");
-    }
-
-    // Takes a HttpServletRequest object with JSON data about a github commit/push.
-    // Extracts and returns the name of the branch that was pushed to.
-    public static String getBranchName(HttpServletRequest request) throws IOException  {
-        String strResponse = JSONtoString(request);
-        String branchName = "";
-
-        // Extract each character from the branch name.
-        // The branch name starts at index 19 in the string, and ends with a quotation mark.
-        int i = 19;
-        char c = strResponse.charAt(i);
-        while (c != '"') {
-            branchName += c;
-            c = strResponse.charAt(i);
-            i++;
-        }
-
-        return branchName;
-    }
-
-    // Turns the JSON request and turns into a String.
-    public static String JSONtoString(HttpServletRequest request) throws IOException {
-        return request.getReader().lines().collect(Collectors.joining());
     }
 
     // used to start the CI server in command line
