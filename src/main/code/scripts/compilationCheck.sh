5ce137d24946c16abb744df594a1bb368ac5f4ad
mnordste <mnordste@kth.se>
2022-02-10 19:51:00 +0100

    forgot to change back scripts

--- src/main/code/scripts/compilationCheck.sh
+++ src/main/code/scripts/compilationCheck.sh
@@ -1,4 +1,7 @@
 cd continuous-integration-java/src
 JETTY_VERSION=7.0.2.v20100331
-echo "javac -cp servlet-api-2.5.jar:jetty-all-$JETTY_VERSION.jar:javax.mail.jar:jakarta.activation.jar \"main/code/ContinuousIntegrationServer.java\" \"main/code/Functions.java\""; then
+if javac -cp servlet-api-2.5.jar:jetty-all-$JETTY_VERSION.jar:javax.mail.jar:jakarta.activation.jar "main/code/ContinuousIntegrationServer.java" "main/code/Functions.java"; then
+    echo "success"
+else
+    echo "fail"
 fi
--- src/main/code/scripts/compileTestsCheck.sh
+++ src/main/code/scripts/compileTestsCheck.sh
@@ -1,4 +1,7 @@
 cd continuous-integration-java/src
 JETTY_VERSION=7.0.2.v20100331
-echo "javac -cp \".:hamcrest.jar:junit.jar:servlet-api-2.5.jar:jakarta.activation.jar:javax.mail.jar:jetty-all-$JETTY_VERSION.jar\" \"main/serverTests/Tests.java\""; then
+if javac -cp ".:hamcrest.jar:junit.jar:servlet-api-2.5.jar:jakarta.activation.jar:javax.mail.jar:jetty-all-$JETTY_VERSION.jar" "main/serverTests/Tests.java"; then
+    echo "success"
+else
+    echo "fail"
 fi
