4b12407f2b1738800bfb2435212c30af329ca7a9
mnordste <mnordste@kth.se>
2022-02-10 15:12:29 +0100

    Fixed #2, doc: added documentation

--- src/main/code/ContinuousIntegrationServer.java
+++ src/main/code/ContinuousIntegrationServer.java
@@ -52,6 +52,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // Check if tests compilation is successful.
         if (Functions.compileTestsCheck()) {
             System.out.println("Tests compiled succesfully.");
+            //If tests compile, run the tests and print the result.
             String testResults = Functions.runTests();
             System.out.println(testResults);
         }
--- src/main/serverTests/Tests.java
+++ src/main/serverTests/Tests.java
@@ -7,36 +7,36 @@ import java.beans.Transient;
 import main.code.*;
 
 import java.io.IOException;
-import java.io.File; 
+import java.io.File;
 
 // This class includes tests for various functions.
 public class Tests {
 
-    @Test 
+    @Test
     public void test_runCommand() throws IOException {
         // Arrange
         String actualOutput = Functions.runCommand("echo testOutput");
 
         // Assert
         String expectedOutput = "testOutput\n";
-        assertTrue(actualOutput.equals(expectedOutput)); 
+        assertTrue(actualOutput.equals(expectedOutput));
     }
 
-    
-    @Test 
+
+    @Test
     public void test_getBranchName() throws IOException {
         // Arrange
         String JSONstring = "{\"ref\":\"refs/heads/someBranchName\",\"before\":\"c747cb43fd0c8564151dc4d1bdbaf7a37cde2638\",\"after\":\"...}";
 
         // Act
         String extractedBranchName = Functions.getBranchName(JSONstring);
-        
+
         // Assert
         String expectedBranchName = "someBranchName";
-        assertTrue(extractedBranchName.equals(expectedBranchName)); 
+        assertTrue(extractedBranchName.equals(expectedBranchName));
     }
 
-    @Test 
+    @Test
     public void test_cloneBranch() throws IOException {
         // Arrange: Delete the old cloned repo
         Functions.deleteClonedRepo();
