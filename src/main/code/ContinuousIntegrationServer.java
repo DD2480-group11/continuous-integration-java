bdddb6299a8bb95b3c01123e628bc20710a9645f
mnordste <mnordste@kth.se>
2022-02-10 19:03:55 +0100

    Relating to #49, Relating to #54, feat: compilation results and testresults should now be added to the email message

--- src/main/code/ContinuousIntegrationServer.java
+++ src/main/code/ContinuousIntegrationServer.java
@@ -62,7 +62,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
             System.out.println("Server compiled succesfully.");
         }
         else {
-            //TODO: add compilation errors
+            //TODO: add compilation errors 
             message.append(codeCompilationResult);
             System.out.println("Server compilation failed.");
         }
