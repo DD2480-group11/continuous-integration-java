0b6ae0cf9936d420d8ef4016e277001199478f28
reversedpeach <reversedpeach@gmail.com>
2022-02-15 23:04:54 +0100

    Fixed #70. feat. Added browsable javadoc format

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"main.code","l":"ContinuousIntegrationServer"},{"p":"main.code","l":"Functions"}];updateSearchResults();
